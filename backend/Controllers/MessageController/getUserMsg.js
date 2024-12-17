18762705adf03e1229c8b84fb6b4980d794e32cb
mr9 asif <asifalibd002@gmail.com>
2024-12-17 23:59:07 +0600

    added chats user and msg

--- backend/Controllers/MessageController/getUserMsg.js
+++ backend/Controllers/MessageController/getUserMsg.js
@@ -2,11 +2,8 @@ const User = require('../../Models/User.model');
 const Message = require('../../Models/messages.model');
 
 const getUserMsg = async(req, res)=>{
-   // const receiverid = req.query.id;
-   // console.log(receiverid)
-   // const user = JSON.parse(req.params.user);
-   // console.log(user)
-   const { userId, receiverId } = req.query;
+    try {
+      const { userId, receiverId } = req.query;
    console.log(userId , receiverId)
     
       const user = await User.findById(receiverId);
@@ -19,6 +16,16 @@ const getUserMsg = async(req, res)=>{
      })
       console.log(user)
       console.log(messages)
+      res.status(201).send({user: user, messages: messages})
+
+      if (!user || !receiverId) {
+         return res.status(404).json({ message: 'User or Receiver not found' });
+     }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+   
 
 
 }
